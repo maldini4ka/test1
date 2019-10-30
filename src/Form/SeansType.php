943069cabcae53da254f6a8c36de3de2c9d6137b
maldini3 <kulbitsky.vn@gmail.com>
2019-10-31 01:28:58 +0300

    31/10-1

---
+++ src/Form/SeansType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Seans;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeansType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('date')
+            ->add('time')
+            ->add('price')
+            ->add('filmid')
+            ->add('hallid')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Seans::class,
+        ]);
+    }
+}
